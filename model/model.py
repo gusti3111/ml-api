8dda05b0949e4a2d55066b4986a11f7c16d5c446
gusti3111 <gusti.mispuansyah@gmail.com>
2024-07-02 00:49:24 +0700

    updatedmodel.py

--- model/model.py
+++ model/model.py
@@ -7,4 +7,26 @@ import pickle
 
 
 data = pd.read_csv("train.csv")
-print(data)
+
+
+#handling missing values with mean
+
+from sklearn.impute import SimpleImputer
+
+imputer =SimpleImputer(strategy="mean")
+data["Age"] = imputer.fit_transform(data[["Age"]])
+
+#Converting categorical data (example: sex) to one-hot encoding
+#One hot encoding is a technique that we use to represent categorical variables as numerical values in a machine learning model.
+
+from sklearn.preprocessing import OneHotEncoder
+
+encoder= OneHotEncoder(sparse_output=False)
+encoded_features = encoder.fit_transform(data[["Sex"]])
+data = pd.concat([data, pd.DataFrame(encoded_features, columns=["Sex_encoded"])], axis=1)
+data.drop("Sex", axis=1, inplace=True)
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+data[["Fare"]] = scaler.fit_transform(data[["Fare"]])
+
+
